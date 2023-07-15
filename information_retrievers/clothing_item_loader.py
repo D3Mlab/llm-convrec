4b503b5c2abf80b078dacfbaaa0ab39caddf4f9c
Kai15116 <kai.iharantonio@gmail.com>
2023-07-14 23:33:04 -0400

    Fixed issues with Clothing Item Loader

--- information_retrievers/clothing_item_loader.py
+++ information_retrievers/clothing_item_loader.py
@@ -3,7 +3,7 @@ from information_retrievers.item_loader import ItemLoader
 import ast
 
 
-class RestaurantItemLoader(ItemLoader):
+class ClothingItemLoader(ItemLoader):
 
     def create_item(self, item_dict: dict) -> Item:
         item_info = {
@@ -11,9 +11,9 @@ class RestaurantItemLoader(ItemLoader):
             'price': item_dict['price'],
             'brand': item_dict['brand'],
             'feature': ast.literal_eval(item_dict['feature']),
-            'rating': float(item_dict['price']),
+            'rating': float(item_dict['rating']),
             'num_reviews': float(item_dict['num_reviews']),
             'rank': item_dict['rank'],
         }
 
-        return Item(item_dict['id'], item_dict['name'], item_info, ast.literal_eval(item_dict['optional']))
+        return Item(item_dict['item_id'], item_dict['name'], item_info, ast.literal_eval(item_dict['optional']))
