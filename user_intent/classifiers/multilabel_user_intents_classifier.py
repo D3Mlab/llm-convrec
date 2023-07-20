f4248314ed44548a301f8466ad1fa3c253d989b2
sarakemper <sara.kemper@icloud.com>
2023-07-20 16:59:20 -0700

    multithread user intents classifier

--- user_intent/classifiers/multilabel_user_intents_classifier.py
+++ user_intent/classifiers/multilabel_user_intents_classifier.py
@@ -4,6 +4,8 @@ from state.state_manager import StateManager
 from user_intent.user_intent import UserIntent
 from user_intent.classifiers.user_intents_classifier import UserIntentsClassifier
 from user_intent.provide_preference import ProvidePreference
+import threading
+from utility.thread_utility import start_thread
 
 
 class MultilabelUserIntentsClassifier(UserIntentsClassifier):
@@ -50,11 +52,29 @@ class MultilabelUserIntentsClassifier(UserIntentsClassifier):
         :return: list of user intents identified
         """
         intent_list = []
+        thread_list = []
         for user_intent in self._user_intents:
-            prompt = user_intent.get_prompt_for_classification(curr_state)
-            str = self._llm_wrapper.make_request(prompt)
-            if "True" in str:
-                intent_list.append(user_intent)
+            
+            thread = threading.Thread(
+                target=self._classify_one_intent, args=(user_intent, curr_state, intent_list))
+            thread_list.append(thread)
+        
+        start_thread(thread_list)
+        
         if self._provide_preference is not None:
             intent_list.append(self._provide_preference)
+        
         return intent_list
+
+    def _classify_one_intent(self, user_intent: UserIntent, curr_state: StateManager, intent_list: list[UserIntent]):
+        """
+        Returns either true/false indicating if the user input should be classified into the user intent.
+        :param curr_state: current state representing the conversation
+        :param user_intent: the user intent that you are trying to see if the user input should be classified into
+        :param intent_list: list of user intents that input is classified into
+        :return: none
+        """
+        prompt = user_intent.get_prompt_for_classification(curr_state)
+        str = self._llm_wrapper.make_request(prompt)
+        if "True" in str:
+            intent_list.append(user_intent)
